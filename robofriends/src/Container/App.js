625aac0ac7175723ced3a8f5804a27cad5398d3f
James Inkoom <James.Inkoom@icloud.com>
2023-05-11 22:51:17 +0000

    refactored code with hooks

--- robofriends/src/Container/App.js
+++ robofriends/src/Container/App.js
@@ -1,51 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import './App.css';
 import Scroll from "../Components/Scroll";
 import ErrorBoundry from "../Components/ErrorBoundry";
 
-class App extends React.Component {
-  constructor () {
-    super ()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App () {
+  // constructor () {
+  //   super ()
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: ''
+  //   }
+  // }
 
-  componentDidMount () {
+  const [robots, setRobots] = useState([])
+  const [ searchfield, setSearchField] = useState('')
+
+  // componentDidMount () {
+  //   fetch ('https://jsonplaceholder.typicode.com/users')
+  //     .then (response => response.json())
+  //     .then (users => {this.setState({ robots: users})});
+  
+  // }
+
+  useEffect(() => {
     fetch ('https://jsonplaceholder.typicode.com/users')
       .then (response => response.json())
-      .then (users => {this.setState({ robots: users})});
+      .then (users => {setRobots(users)});
+  }, []) // an empty array [] is a short cut for componentdidMount when using a class
   
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value)
+    // this.setState({ searchfield: event.target.value }) use this when using a class component.
+
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
+  const filteredRobot = robots.filter(robots => {
+    return robots.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
+  })
 
-  }
-
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobot = robots.filter(robots => {
-      return robots.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
-    })
-
-    return !robots.length ?
-      <h1>Loading</h1> :
-        (
-          <div className="tc">
-            <h1 className="f1">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-              <ErrorBoundry>
-                <CardList robots={filteredRobot} />
-              </ErrorBoundry>
-            </Scroll>
-          </div>
-        );
-    }
+  return !robots.length ?
+    <h1>Loading</h1> :
+      (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={onSearchChange}/>
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobot} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
   }
 
 
